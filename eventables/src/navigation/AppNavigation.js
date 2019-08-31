730b76f572e2daa60c8034bf18d591c39b033219
Asim Poptani <asim@poptani.com>
2019-08-31 18:25:49 +0100

    Added a tranistion between screens
    Signed-off-by: Asim Poptani <asim@poptani.com>

--- eventables/src/navigation/AppNavigation.js
+++ eventables/src/navigation/AppNavigation.js
@@ -1,10 +1,12 @@
 import React from 'react';
 // Navigation
 import {
-  createSwitchNavigator,
   createAppContainer,
 } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import createAnimatedSwitchNavigator from 'react-navigation-animated-switch';
+import { Transition } from 'react-native-reanimated';
+
 // Screens
 import Home from '../screens/Home';
 import SplashScreen from '../screens/SplashScreen';
@@ -21,9 +23,21 @@ const MainAppNavigation = createStackNavigator({
   ExpandedView: {screen: ExpandedView},
 });
 // Switch navigator to switch from Splashscreen to stack
-const SplashScreenToMainAppNavigation = createSwitchNavigator({
+const SplashScreenToMainAppNavigation = createAnimatedSwitchNavigator({
   SplashScreen: {screen: SplashScreen},
   MainAppNavigation,
+},{
+  // Added transition between SplashScreen and mainApplication
+  transition: (
+    <Transition.Together>
+      <Transition.Out
+        type="slide-bottom"
+        durationMs={400}
+        interpolation="easeIn"
+      />
+      <Transition.In type="fade" durationMs={500} />
+    </Transition.Together>
+  ),
 });
 // return App navigator
 export default createAppContainer(SplashScreenToMainAppNavigation)
